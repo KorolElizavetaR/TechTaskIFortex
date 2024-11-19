b251253d6d994809a0857c79ba6b6bade112f8b4
KorolER <elizavetakorol2005@gmail.com>
2024-11-19 13:04:14 +0300

    Techtask (#1)
    * Implement getBooks
    
    Implement getBooks in ESBookServiceImpl (NOTICE! Requires order fixing)
    
    * Implement getBooks()
    
    Changed Criteria to native query to end up with ordered result.
    
    * Implement getAllByCriteria
    
    Implement getAllByCriteria from ESBookServiceImpl
    
    * Implement findMember. Change in .gitignore
    
    Implement findMember from ESMemberService
    
    * TechTask is done

--- src/main/java/com/ifortex/bookservice/service/impl/ESBookServiceImpl.java
+++ src/main/java/com/ifortex/bookservice/service/impl/ESBookServiceImpl.java
@@ -3,24 +3,92 @@ package com.ifortex.bookservice.service.impl;
 import com.ifortex.bookservice.dto.SearchCriteria;
 import com.ifortex.bookservice.model.Book;
 import com.ifortex.bookservice.service.BookService;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Predicate;
+import jakarta.persistence.criteria.Root;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
-// Attention! It is FORBIDDEN to make any changes in this file!
+/* Attention! It is FORBIDDEN to make any changes in this file! 
+ * 	I might be wrong, but maybe this comment belongs to Controller's classes?*/
+
+@Transactional
+@RequiredArgsConstructor
 @Service
 public class ESBookServiceImpl implements BookService {
+	@PersistenceContext
+	private final EntityManager em;
 
-  @Override
-  public Map<String, Long> getBooks() {
-    // will be implemented shortly
-    return Map.of();
-  }
+	@Override
+	public Map<String, Long> getBooks() {
+		String nativeSQL = """
+				    SELECT genre_dist AS genre, COUNT(*) AS counter
+				    FROM books, UNNEST(genre) AS genre_dist
+				    GROUP BY genre_dist
+				    ORDER BY counter DESC
+				""";
 
-  @Override
-  public List<Book> getAllByCriteria(SearchCriteria searchCriteria) {
-    // will be implemented shortly
-    return List.of();
-  }
+		return (Map<String, Long>) em.createNativeQuery(nativeSQL).getResultList().stream()
+				.collect(Collectors.toMap(result -> (String) ((Object[]) result)[0],
+						result -> (Long) ((Object[]) result)[1], (oldValue, newValue) -> oldValue, LinkedHashMap::new));
+	}
+
+	@Override
+	public List<Book> getAllByCriteria(SearchCriteria searchCriteria) {
+		CriteriaBuilder cb = em.getCriteriaBuilder();
+
+		CriteriaQuery<Book> cr = cb.createQuery(Book.class);
+		Root<Book> root = cr.from(Book.class);
+
+		List<Predicate> predicate = new ArrayList<>();
+
+		String title = searchCriteria.getTitle();
+		if (title != null) {
+			title = title.trim();
+			predicate.add(cb.like(root.get("title"), String.format("%%%s%%", title)));
+		}
+
+		String author = searchCriteria.getAuthor();
+		if (author != null) {
+			author = author.trim();
+			predicate.add(cb.like(root.get("author"), String.format("%%%s%%", author)));
+		}
+
+		String genre = searchCriteria.getGenre();
+		if (genre != null) {
+			predicate.add(cb.like(
+					cb.concat("|", cb.concat(
+							cb.function("array_to_string", String.class, root.get("genres"), cb.literal("|")), "|")),
+					cb.literal(String.format("%%|%s|%%", genre))));
+		}
+
+		String description = searchCriteria.getDescription();
+		if (description != null) {
+			description = description.trim();
+			predicate.add(cb.like(root.get("description"), String.format("%%%s%%", description)));
+		}
+
+		Integer year = searchCriteria.getYear();
+		if (year != null) {
+			LocalDateTime startOfYear = LocalDateTime.of(year, 1, 1, 0, 0, 0);
+			LocalDateTime endOfYear = LocalDateTime.of(year, 12, 31, 23, 59, 59);
+			predicate.add(cb.between(root.get("publicationDate"), startOfYear, endOfYear));
+		}
+
+		cr.select(root).where(cb.and(predicate.toArray(new Predicate[0])));
+		return em.createQuery(cr).getResultList();
+	}
 }
--- src/main/java/com/ifortex/bookservice/service/impl/ESMemberServiceImpl.java
+++ src/main/java/com/ifortex/bookservice/service/impl/ESMemberServiceImpl.java
@@ -1,24 +1,74 @@
 package com.ifortex.bookservice.service.impl;
 
+import com.ifortex.bookservice.model.Book;
 import com.ifortex.bookservice.model.Member;
 import com.ifortex.bookservice.service.MemberService;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Join;
+import jakarta.persistence.criteria.JoinType;
+import jakarta.persistence.criteria.Order;
+import jakarta.persistence.criteria.Root;
+import jakarta.persistence.criteria.Subquery;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 // Attention! It is FORBIDDEN to make any changes in this file!
+/* Same commentary as in ESBookServiceImpl */
+
 @Service
+@Transactional
+@RequiredArgsConstructor
 public class ESMemberServiceImpl implements MemberService {
+	@PersistenceContext
+	private final EntityManager em;
 
-  @Override
-  public Member findMember() {
-    // will be implemented shortly
-    return null;
-  }
+	@Override
+	public Member findMember() {
+		String genre = "Romance";
+		CriteriaBuilder cb = em.getCriteriaBuilder();
+		CriteriaQuery<Member> cq = cb.createQuery(Member.class);
+		Root<Member> memberRoot = cq.from(Member.class);
+		Join<Member, Book> joinMemberBook = memberRoot.join("borrowedBooks", JoinType.INNER);
 
-  @Override
-  public List<Member> findMembers() {
-    // will be implemented shortly
-    return List.of();
-  }
+		Subquery<Long> genreQuery = cq.subquery(Long.class);
+		Root<Book> bookRoot = genreQuery.from(Book.class);
+		genreQuery.select(bookRoot.get("id"))
+				.where(cb.like(cb.concat("|", cb.concat(
+						cb.function("array_to_string", String.class, bookRoot.get("genres"), cb.literal("|")), "|")),
+						cb.literal(String.format("%%|%s|%%", genre))));
+
+		List<Order> orders = new ArrayList<>();
+		orders.add(cb.asc(joinMemberBook.get("publicationDate")));
+		orders.add(cb.desc(memberRoot.get("membershipDate")));
+
+		cq.select(memberRoot).where(cb.in(joinMemberBook.get("id")).value(genreQuery)).orderBy(orders);
+		return em.createQuery(cq).setMaxResults(1).getSingleResult();
+	}
+
+	@Override
+	public List<Member> findMembers() {
+		int regYear = 2023;
+		LocalDateTime startOfYear = LocalDateTime.of(regYear, 1, 1, 0, 0, 0);
+		LocalDateTime endOfYear = LocalDateTime.of(regYear, 12, 31, 23, 59, 59);
+
+		CriteriaBuilder cb = em.getCriteriaBuilder();
+		CriteriaQuery<Member> query = cb.createQuery(Member.class);
+		Root<Member> root = query.from(Member.class);
+		Join<Member, Book> joinMemberBook = root.join("borrowedBooks", JoinType.LEFT);
+
+		query.select(root).where(cb.and(cb.between(root.get("membershipDate"), startOfYear, endOfYear),
+				cb.isNull(joinMemberBook.get("id"))));
+
+		return em.createQuery(query).getResultList();
+	}
 }
